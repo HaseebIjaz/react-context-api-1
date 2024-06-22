289eedaaea54220435342c949e0428a2b7c93dca
Haseeb Ijaz <haseebijazi150172@gmail.com>
2024-06-22 16:02:48 +0500

    React COntext API Baics

---
+++ react-context-api-1/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import UserContextProvider from "./context/UserContextProvider";
+
+function App() {
+  return (
+    <UserContextProvider>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  );
+}
+
+export default App;
---
+++ react-context-api-1/src/context/UserContext.js
@@ -0,0 +1,8 @@
+//Part 1
+// import React from "react";
+// const UserContext = React.createContext();
+// export default UserContext;
+
+import React from "react";
+const UserContext = React.createContext();
+export default UserContext;
